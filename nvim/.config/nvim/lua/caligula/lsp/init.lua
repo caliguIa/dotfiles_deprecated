517d9bb28795b2dffd0a7bc5c72b8750a6bf6f93
caligula <calrichards@me.com>
2022-08-01 16:27:58 +0100

    commit

--- nvim/.config/nvim/lua/caligula/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
-require "caligula.lsp.configs"
-require("caligula.lsp.handlers").setup()
-require "caligula.lsp.null-ls"
