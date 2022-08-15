25938678224bbedce1caf39fa54571c041e5bca0
caligula <calrichards@me.com>
2022-08-15 22:40:25 +0100

    commit

--- sketchybar/.config/sketchybar/plugins/space.sh
+++ sketchybar/.config/sketchybar/plugins/space.sh
@@ -1,28 +1,11 @@
 #!/usr/bin/env bash
 
-update() {
 if [ "$SELECTED" = "true" ]; then
-  sketchybar -m --set $NAME label.highlight=on icon.highlight=on background.drawing=on
+  args+=(--set $NAME icon.background.y_offset=-12)
 else
-  sketchybar -m --set $NAME label.highlight=off icon.highlight=off background.drawing=off
+  args+=(--set $NAME icon.background.y_offset=-20)
 fi
-}
 
-mouse_entered() {
-  sketchybar -m --set $NAME icon.highlight=on \
-                            label.highlight=on
-}
+args+=(icon.highlight=$SELECTED)
 
-mouse_exited() {
-  sketchybar -m --set $NAME icon.highlight=off \
-                            label.highlight=off
-}
-
-case "$SENDER" in
-  "mouse.entered") mouse_entered
-  ;;
-  "mouse.exited") mouse_exited
-  ;;
-  *) update 
-  ;;
-esac
+sketchybar --animate tanh 20 "${args[@]}"
