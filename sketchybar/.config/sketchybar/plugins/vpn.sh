18b687802b33f7cbd774bf617c759ad508815a24
caligula <calrichards@me.com>
2022-08-15 23:23:34 +0100

    commit

--- sketchybar/.config/sketchybar/plugins/vpn.sh
+++ sketchybar/.config/sketchybar/plugins/vpn.sh
@@ -3,11 +3,11 @@
 VPN=$(mullvad status)
 
 if [ "$VPN" = "Disconnected" ]; then
-  mullvad connect
-  # sketchybar -m --set net_logo icon= \
-  #   icon.color=0xff63E473
+  # mullvad connect
+  sketchybar -m --set net_logo icon= \
+    icon.color=0xff63E473
 else
-  mullvad disconnect
-  # sketchybar -m --set net_logo icon= \
-  #   icon.color=0xffffffff
+  # mullvad disconnect
+  sketchybar -m --set net_logo icon= \
+    icon.color=0xffffffff
 fi
