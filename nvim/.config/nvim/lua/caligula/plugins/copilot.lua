691f452149bad2df4aa494b5ebeef972090eaaf7
caligula <calrichards@me.com>
2022-11-24 12:22:53 +0000

    copilot autostart nvim

--- nvim/.config/nvim/lua/caligula/plugins/copilot.lua
+++ nvim/.config/nvim/lua/caligula/plugins/copilot.lua
@@ -1,42 +1,29 @@
 local setup, copilot = pcall(require, "copilot")
 if not setup then
-  return
+	return
 end
 
 copilot.setup({
-  panel = {
-    enabled = true,
-    auto_refresh = false,
-    keymap = {
-      jump_prev = "[[",
-      jump_next = "]]",
-      accept = "<CR>",
-      refresh = "gr",
-      open = "<M-CR>"
-    },
-  },
-  suggestion = {
-    enabled = true,
-    auto_trigger = false,
-    debounce = 75,
-    keymap = {
-     accept = "<C-a>",
-     next = "<M-]>",
-     prev = "<M-[>",
-     dismiss = "<C-]>",
-    },
-  },
-  filetypes = {
-    yaml = false,
-    markdown = false,
-    help = false,
-    gitcommit = false,
-    gitrebase = false,
-    hgcommit = false,
-    svn = false,
-    cvs = false,
-    ["."] = false,
-  },
-  copilot_node_command = 'node', -- Node version must be < 18
-  server_opts_overrides = {},
+	suggestion = {
+		enabled = true,
+		auto_trigger = true,
+		debounce = 75,
+		keymap = {
+			accept = "<C-a>",
+			next = "<M-]>",
+			prev = "<M-[>",
+			dismiss = "<C-]>",
+		},
+	},
+	filetypes = {
+		help = false,
+		gitcommit = false,
+		gitrebase = false,
+		hgcommit = false,
+		svn = false,
+		cvs = false,
+		["."] = false,
+	},
+	copilot_node_command = "node", -- Node version must be < 18
+	server_opts_overrides = {},
 })
